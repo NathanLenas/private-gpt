ad1bc39d981a4ef1d585f1fc60be2724c68cc31e
Nathan Lenas <nathan.lenas@lmddc.lu>
2024-08-13 14:51:52 +0200

    doc filtering

--- private_gpt/open_ai/extensions/context_filter.py
+++ private_gpt/open_ai/extensions/context_filter.py
@@ -5,3 +5,9 @@ class ContextFilter(BaseModel):
     docs_ids: list[str] | None = Field(
         examples=[["c202d5e6-7b69-4869-81cc-dd574ee8ee11"]]
     )
+    docs_priorities: list[str] | None = Field(
+        examples=[["high", "middle"]]
+    )
+    docs_categories: list[str] | None = Field(
+        examples=[["Other"]]
+    )
